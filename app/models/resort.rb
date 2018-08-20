0e79c00efa8ecfd48e3c395fb31a2103513dfa32
Adrian Wright <agwright72@gmail.com>
2018-08-20 12:19:11 +0700

    models created

---
+++ app/models/resort.rb
@@ -0,0 +1,4 @@
+class Resort < ApplicationRecord
+  belongs_to :user
+  belongs_to :airport
+end
---
+++ db/migrate/20180820051249_create_resorts.rb
@@ -0,0 +1,18 @@
+class CreateResorts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resorts do |t|
+      t.string :name
+      t.string :continent
+      t.string :country
+      t.string :description
+      t.string :address
+      t.string :website_link
+      t.string :contact_link
+      t.string :booking_link
+      t.references :user, foreign_key: true
+      t.references :airport, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
